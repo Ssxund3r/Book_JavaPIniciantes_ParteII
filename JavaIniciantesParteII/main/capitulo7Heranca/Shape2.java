452c662d0fa11d20a1126cca297c6aec2418c198
Ssxund3r <https://github.com/Ssxund3r>
2022-09-10 15:55:07 -0300

    Refatoração Classes

--- JavaIniciantesParteII/main/capitulo7Heranca/Shape2.java
+++ JavaIniciantesParteII/main/capitulo7Heranca/Shape2.java
@@ -2,28 +2,20 @@ package capitulo7Heranca;
 
 public class Shape2 {
 	public static void main(String[] args) {
-		Triangle t1 = new Triangle();
-		Triangle t2 = new Triangle();
+		Triangle t1 = new Triangle("filled", 4.0, 4.0);
+		Triangle t2 = new Triangle("outlined", 8.0, 12.0);
 
-		t1.setWidth(4.0);
-		t1.setHeight(4.0);
-		t1.style = "filled";
-
-		t2.setWidth(8.0);
-		t2.setHeight(12.0);
-		t2.style = "outlined";
-		
 		System.out.println("Into for t1: ");
 		t1.showStyle();
 		t1.showDim();
 		System.out.println("Area is : " + t1.area());
-		
+
 		System.out.println();
-		
+
 		System.out.println("Into for t2: ");
 		t2.showStyle();
 		t2.showDim();
 		System.out.println("Area is : " + t2.area());
-		
+
 	}
 }
--- JavaIniciantesParteII/main/capitulo7Heranca/Triangle.java
+++ JavaIniciantesParteII/main/capitulo7Heranca/Triangle.java
@@ -1,12 +1,19 @@
 package capitulo7Heranca;
+
 //uma subclasse de TwoDShape para triângulos
 public class Triangle extends TwoDShape {
 	String style;
+
+	public Triangle(String s, double h, double w) {
+		setWidth(w);
+		setHeight(h);
+		style = s;
+	}
 	
 	double area() {
 		return getWidth() * getHeight() / 2;
 	}
-	
+
 	void showStyle() {
 		System.out.println("Triangle is " + style);
 	}
