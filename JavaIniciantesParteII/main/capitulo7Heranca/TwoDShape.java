1359e259fbedf56e1826533e84e0cddb3a3231e0
Ssxund3r <https://github.com/Ssxund3r>
2022-09-10 12:43:01 -0300

    Classe para duas dimensões.

---
+++ JavaIniciantesParteII/main/capitulo7Heranca/TwoDShape.java
@@ -0,0 +1,15 @@
+package capitulo7Heranca;
+
+//Uma hierarquia de calsse simples.
+//Uma classe para objetos de duas dimensões. 
+public class TwoDShape {
+	double width;
+	double height;
+
+	void showDim() {
+		System.out.println("Width and height are " + width + 
+				" and " + height);
+	}
+	
+	
+}
